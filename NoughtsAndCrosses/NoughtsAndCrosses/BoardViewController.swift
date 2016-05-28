459cf06079be844e17408e34acb7da056e1e14dd
castillejoale <castillejo.alejandro@gmail.com>
2016-05-28 17:35:12 +0200

    Base code

---
+++ NoughtsAndCrosses/NoughtsAndCrosses/BoardViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BoardViewController.swift
+//  NoughtsAndCrosses
+//
+//  Created by Alejandro Castillejo on 5/27/16.
+//  Copyright © 2016 Julian Hulme. All rights reserved.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
