e792622349fde2a55c795bbf239b9ee7bbd26504
Ryan Davey <ryandavey@college.harvard.edu>
2016-06-02 04:09:22 +0200

    assignment 4b update

---
+++ NoughtsAndCrosses/NoughtsAndCrosses/EasterEggController.swift
@@ -0,0 +1,9 @@
+//
+//  EasterEggController.swift
+//  NoughtsAndCrosses
+//
+//  Created by Ryan Davey on 6/1/16.
+//  Copyright © 2016 Julian Hulme. All rights reserved.
+//
+
+import Foundation
---
+++ NoughtsAndCrosses/NoughtsAndCrosses/EasterEggViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EasterEggViewController.swift
+//  NoughtsAndCrosses
+//
+//  Created by Ryan Davey on 6/1/16.
+//  Copyright © 2016 Julian Hulme. All rights reserved.
+//
+
+import UIKit
+
+class EasterEggViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
